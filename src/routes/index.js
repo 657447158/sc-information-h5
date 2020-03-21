0d6eeb7adb22c49ac8261d667883e75caac3adfd
657447158 <657447158@qq.com>
2020-03-21 22:03:45 +0800

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,6 @@ import routes from './common'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-    mode: 'history',
     routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,5 @@ module.exports = {
             patterns: []
         }
     },
-    publicPath: '/',
+    publicPath: './',
 }
